4a0c30ea4105ae8f75fc09669ca033c71566ac56
Thomas Utsey <tom@edgegluer.com>
2020-03-14 11:52:06 -0400

    Updates

---
+++ precache-manifest.cfe0d8a0379ed7c1f8de57df3fdde740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbed5b92789646fa436a820f14f12bb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6496eceb74b8f180d7d",
+    "url": "/static/css/main.6b6a3b59.chunk.css"
+  },
+  {
+    "revision": "69551a4546efd683cff3",
+    "url": "/static/js/2.ad140b2d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ad140b2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6496eceb74b8f180d7d",
+    "url": "/static/js/main.86b4de57.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  }
+]);
